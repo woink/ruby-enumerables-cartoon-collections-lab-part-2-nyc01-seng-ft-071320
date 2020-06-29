6d0483747b8829330495866b642e494a9b731906
Ward Price <wardprice@gmail.com>
2020-06-29 14:58:27 +0000

    Automatically backed up by Learn

--- cartoon_collections.rb
+++ cartoon_collections.rb
@@ -13,7 +13,6 @@ def long_planeteer_calls(planeteer_calls)
 end
 
 def find_valid_calls(planeteer_calls)
-  
   valid_calls = ["Earth!", "Wind!", "Fire!", "Water!", "Heart!"]
-  planeteer_calls.find { |call| call.include? valid_calls }
+  planeteer_calls.find { |call| valid_calls.include? call }
 end
